7de72f4e78ed6cc7664e3e2d04c9f282d4179843
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-21 19:27:39 -0300

    refactor: arrumar main

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 //
 // Created by Erick Gomes on 21/05/24.
 //
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
+#include <string.h>
 #include "quicksort.h"
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2) {
-        printf("Usage: %s <num_elements> <element1> <element2> ... <elementN>\n", argv[0]);
-        return 1;
-    }
-    int n = atoi(argv[1]);
+int main() {
+    int n;
+    scanf("%d\n", &n);
+    char elements[n+1];
+    fgets(elements, sizeof(elements), stdin);
+    
     int arr[n];
+
     for (int i = 0; i < n; i++) {
-        arr[i] = atoi(argv[i + 2]);
+        arr[i] = elements[i] - '0';
     }
+
+    clock_t start = clock();
+
     quickSortIterative(arr, 0, n - 1);
+
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
     printArr(arr, n);
+
+    printf("\nTempo de execução: %f segundos\n", time_taken);
+
     return 0;
 }
